5ba7ae600fdc645b41525914903888996da01281
liao <a88002614@gmail.com>
2024-12-29 21:19:40 +0800

    upload

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
-module.exports = defineConfig({
-  publicPath: '/liao42.github.io/dist/'
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/liao42.github.io/dist/'
+    : '/'
+}
